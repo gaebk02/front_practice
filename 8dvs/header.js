2dd2e4e92af4d72c3557cc9a416350ad74c36f50
Taekyun <gaebk02@gmail.com>
2024-06-02 13:35:46 +0900

    slide修正

--- 8dvs/header.js
+++ 8dvs/header.js
@@ -3,16 +3,16 @@ window.onload = function () {
 };
 
 function infiniteSlide() {
-  var slide = document.getElementById("infinite-slide");
-  var slideWidth = slide.offsetWidth;
-  var windowWidth = window.innerWidth;
-
-  var numSlides = Math.ceil(windowWidth / slideWidth) + 30; // 화면 너비를 넘어서는 만큼 추가로 반복할 횟수 계산
-  var newContent = "";
-
-  for (var i = 0; i < numSlides; i++) {
-    newContent += slide.innerHTML; // 문구 반복해서 newContent에 추가
+  let slideContents = document.getElementsByClassName(
+    "infinite-slider-content"
+  );
+  for (let i = 0; i < slideContents.length; i++) {
+    let slide = slideContents[i];
+    let numSlides = 30;
+    let newContent = "";
+    for (let i = 0; i < numSlides; i++) {
+      newContent += slide.innerHTML;
+    }
+    slide.innerHTML = newContent;
   }
-
-  slide.innerHTML = newContent; // 반복된 문구를 요소에 설정
 }
